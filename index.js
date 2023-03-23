c5b0fe9744456c66bd4c7284c56a8afdf22fc4cb
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-03-23 15:23:20 -0300

    Remoção comentários

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ soap.createClient(url, function (err, client) {
     in2: new Date(Date.now()).toLocaleString().split(',')[0],
   }, function (err, result) {
     if (err) return console.log(err);
-    //console.log(result.getUltimoValorVOReturn.ultimoValor.svalor['$value']);getValorEspecial
-    //console.log(result.getValoresSeriesVOReturn.getValoresSeriesVOReturn.valores);
     writeJsonFile('valoresVOReturn', result.getValoresSeriesVOReturn.getValoresSeriesVOReturn.valores )
   });
 });
@@ -26,26 +24,3 @@ function writeJsonFile(fileName, fileData){
     console.log(`${fileName} Dados salvos com sucesso!`)
   })
 }
-
-//wsdl request
-/*var client = new XMLHttpRequest();
-client.open('GET', 'https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl');
-client.onreadystatechange = function () {
-  var request = client.responseText;//here the wsdl
-
-        //SOAP request
-        var client2 = new XMLHttpRequest();
-        client2.open('POST', 'http://83.212.96.238:8080/DgesvSampleWs/DgesvSampleWsService', true);
-
-        client2.onreadystatechange = function () {
-          if (client2.readyState == 4) {
-            if (client2.status == 200) {
-              console.log(client.responseText);//here the response
-            }
-          }
-        }
-        client2.setRequestHeader('Content-Type', 'text/xml');
-        client2.send(request);
-}
-
-client.send();*/
